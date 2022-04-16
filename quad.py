d0d85f1af3ab26e520dc56eb2ab302fbe75a500c
Alex Eidt <alex.eidt@outlook.com>
2022-04-16 09:47:59 -0700

    Linted quad.py

--- quad.py
+++ quad.py
@@ -17,6 +17,7 @@ from tqdm import tqdm
 MIN_WIDTH = 10
 MIN_HEIGHT = 10
 
+
 def border(image):
     """
     Add a black border around the given image quadrant.
@@ -32,9 +33,9 @@ def border(image):
     half_w = w // 2
     half_h = h // 2
     # Horizontal Line
-    image[half_h:half_h+1, :] = 0
+    image[half_h : half_h + 1, :] = 0
     # Vertical Line
-    image[:, half_w:half_w+1] = 0
+    image[:, half_w : half_w + 1] = 0
 
 
 def rgb_mean(image):
@@ -66,7 +67,7 @@ def quad(iterations, image, edited, quads, set_border=True):
     Split the given image into four quadrants.
     Update the edited image by coloring in the newly split quadrants to the average rgb
     color of the original image.
-    Find the quadrant with the maximum error, remove it from the "quads" list and return it. 
+    Find the quadrant with the maximum error, remove it from the "quads" list and return it.
     """
     if iterations <= 0:
         return image, edited
@@ -86,10 +87,10 @@ def quad(iterations, image, edited, quads, set_border=True):
             edited[:half_h, :half_w] = rgb_mean(top_left)
             edited[:half_h, half_w:] = rgb_mean(top_right)
             edited[half_h:, :half_w] = rgb_mean(bottom_left)
-            edited[half_h:, half_w:] = rgb_mean(bottom_right)     
+            edited[half_h:, half_w:] = rgb_mean(bottom_right)
 
             if set_border:
-                border(edited)   
+                border(edited)
 
             quads.append((error(top_left), top_left, edited[:half_h, :half_w]))
             quads.append((error(top_right), top_right, edited[:half_h, half_w:]))
@@ -109,18 +110,47 @@ def quad(iterations, image, edited, quads, set_border=True):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Quadtree Image Segmentation.')
-    parser.add_argument('input', type=str, help='Image to segment.')
-    parser.add_argument('output', type=str, help='Output filename.')
-    parser.add_argument('-fps', type=int, default=1, help='Output FPS.')
-    parser.add_argument('-q', '--quality', type=int, default=5, help='Quality of the output video.')
-    parser.add_argument('-a', '--animate', action='store_true', help='Save intermediary frames as video.')
-    parser.add_argument('-i', '--iterations', type=int, default=12, help='Number of iterations.')
-    parser.add_argument('-b', '--border', action='store_true', help='Add borders to subimages.')
-    parser.add_argument('-img', '--image', action='store_true', help='Save final output image.')
-    parser.add_argument('-s', '--step', type=float, default=2.0, help='Only save a frame every `s^(iteration)` iterations. For use with --animate only.')
-    parser.add_argument('-f', '--frames', action='store_true', help='Save intermediary frames as images.')
-    parser.add_argument('-au', '--audio', action='store_true', help='Add audio from the input file to the output file.')
+    parser = argparse.ArgumentParser(description="Quadtree Image Segmentation.")
+    parser.add_argument("input", type=str, help="Image to segment.")
+    parser.add_argument("output", type=str, help="Output filename.")
+    parser.add_argument("-fps", type=int, default=1, help="Output FPS.")
+    parser.add_argument(
+        "-q", "--quality", type=int, default=5, help="Quality of the output video."
+    )
+    parser.add_argument(
+        "-a",
+        "--animate",
+        action="store_true",
+        help="Save intermediary frames as video.",
+    )
+    parser.add_argument(
+        "-i", "--iterations", type=int, default=12, help="Number of iterations."
+    )
+    parser.add_argument(
+        "-b", "--border", action="store_true", help="Add borders to subimages."
+    )
+    parser.add_argument(
+        "-img", "--image", action="store_true", help="Save final output image."
+    )
+    parser.add_argument(
+        "-s",
+        "--step",
+        type=float,
+        default=2.0,
+        help="Only save a frame every `s^(iteration)` iterations. For use with --animate only.",
+    )
+    parser.add_argument(
+        "-f",
+        "--frames",
+        action="store_true",
+        help="Save intermediary frames as images.",
+    )
+    parser.add_argument(
+        "-au",
+        "--audio",
+        action="store_true",
+        help="Add audio from the input file to the output file.",
+    )
     args = parser.parse_args()
 
     # Try to load an image from the given input. If this fails, assume it's a video.
@@ -131,15 +161,17 @@ def main():
         with imageio.read(args.input) as video:
             data = video.get_meta_data()
 
-            kwargs = {'fps': data['fps'], 'quality': min(max(args.quality, 0), 10)}
+            kwargs = {"fps": data["fps"], "quality": min(max(args.quality, 0), 10)}
             if args.audio:
-                kwargs['audio_path'] = args.input
+                kwargs["audio_path"] = args.input
 
-            writer = imageio_ffmpeg.write_frames(args.output, data['source_size'], **kwargs)
+            writer = imageio_ffmpeg.write_frames(
+                args.output, data["source_size"], **kwargs
+            )
             writer.send(None)
 
             quads = []
-            for frame in tqdm(video, total=int(data['fps'] * data['duration'] + 0.5)):
+            for frame in tqdm(video, total=int(data["fps"] * data["duration"] + 0.5)):
                 copy = frame.copy()
                 edited = copy
                 quad(args.iterations, frame, edited, quads, set_border=args.border)
@@ -158,7 +190,13 @@ def main():
         if args.animate:
             with imageio.save(args.output, fps=args.fps) as writer:
                 for i in tqdm(range(args.iterations)):
-                    image, edited = quad(int(args.step ** i), image, edited, quads, set_border=args.border)
+                    image, edited = quad(
+                        int(args.step**i),
+                        image,
+                        edited,
+                        quads,
+                        set_border=args.border,
+                    )
 
                     writer.append_data(copy)
                     if args.frames:
@@ -171,5 +209,5 @@ def main():
             imageio.imsave(args.output, copy)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
